a1195f13649a5c0a5fa826d3422b8ed2e4f9bc43
Derek Pollard <dpollard@mail.bradley.edu>
2020-10-09 11:54:03 -0500

    Add in cleaner homepage

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -9,7 +9,9 @@ export function HomePage() {
         <meta name="description" content="A Boilerplate application homepage" />
       </Helmet>
       <div className="flex flex-col sm:flex-row items-center justify-center h-full text-5xl space-x-0 sm:space-x-4">
-        <div>Welcome to your React Interview</div>
+        <div>
+          Welcome to your <span className="text-blue-400">React</span> interview
+        </div>
       </div>
     </>
   );
