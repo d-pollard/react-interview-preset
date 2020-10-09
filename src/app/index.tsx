845b9d26f09e6c007cdbd3479982a003033f3790
Derek Pollard <dpollard@mail.bradley.edu>
2020-10-09 10:49:44 -0500

    Add in tailwind; remove boilerplate; start fresh

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,26 +10,27 @@ import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
-import { GlobalStyle } from '../styles/global-styles';
-
 import { HomePage } from './containers/HomePage/Loadable';
-import { NotFoundPage } from './containers/NotFoundPage/Loadable';
+import { NotFoundPage } from './components/NotFoundPage/Loadable';
 
 export function App() {
   return (
     <BrowserRouter>
       <Helmet
-        titleTemplate="%s - React Boilerplate"
-        defaultTitle="React Boilerplate"
+        titleTemplate="%s - React Interview Starter"
+        defaultTitle="React Interview Starter"
       >
-        <meta name="description" content="A React Boilerplate application" />
+        <meta
+          name="description"
+          content="A great starting point for any React interview"
+        />
       </Helmet>
 
+      <div className="text-pink-50">Hello fellow friends</div>
       <Switch>
-        <Route exact path={process.env.PUBLIC_URL + '/'} component={HomePage} />
+        <Route exact path="/" component={HomePage} />
         <Route component={NotFoundPage} />
       </Switch>
-      <GlobalStyle />
     </BrowserRouter>
   );
 }
